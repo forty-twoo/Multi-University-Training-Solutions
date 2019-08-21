af8a98feb251407f31738491c966f12bd33b20de
forty-twoo <43334171+forty-twoo@users.noreply.github.com>
2019-08-21 18:52:52 +0800

    权值线段树

--- HDU-03/1007.cpp
+++ HDU-03/1007.cpp
@@ -2,7 +2,7 @@
  * @Don't panic!
  * @Author: forty-twoo
  * @Date: 2019-08-21 13:36
- * @Description: 主席树 
+ * @Description: 权值线段树
  * @Source: http://acm.hdu.edu.cn/showproblem.php?pid=6609
  */
 
@@ -17,74 +17,76 @@ typedef long long ll;
 typedef  unsigned long long ull;
 using namespace std;
 const int MAX = 2e5+10;
-const int MAXN=MAX*20;
+const int MAXN=MAX;
 const int MOD = 998244353;
-int ks,n,rt[MAX];
-ll w[MAX],m;
+int ks,n;
+ll w[MAX],k;
 ll dc[MAX],tot=0;
-struct ChairmanT{
-	int lc[MAXN],rc[MAXN],cnt=0,num[MAXN];ll sum[MAXN];
-	void init(){
-		cnt=0;
-	}
-	void build(int &o,int l,int r){
-		o=++cnt;
-		if(l==r){
-			sum[o]=num[o]=lc[o]=rc[o]=0;return;
-		}
-		int mid=(l+r)>>1;
-		build(lc[o],l,mid);build(rc[o],mid+1,r);
-	}
-	void insert(int &o,int pre,int l,int r,int p){
-		o=++cnt;lc[o]=lc[pre],rc[o]=rc[pre],sum[o]=sum[pre]+dc[p];num[o]=num[pre]+1;
-		if(l==r) return;
-		int mid=(l+r)>>1;
-		if(p<=mid) insert(lc[o],lc[pre],l,mid,p);
-		else insert(rc[o],rc[pre],mid+1,r,p);
-	}
-	int query(int o,int pre,int l,int r,ll sm){
-		if(l==r) {
-			if(sum[o] && num[o]){
-				ll v=sum[o]/num[o];
-				return min(num[o],(int)(sm/v));  //注意此处可选的最多数量
-			} else return 0;
-		}
-		int mid=(l+r)>>1;
-		ll delt=sum[lc[o]]-sum[lc[pre]];
-		if(delt<=sm) return num[lc[o]]-num[lc[pre]]+query(rc[o],rc[pre],mid+1,r,sm-delt);
-		else return query(lc[o],lc[pre],l,mid,sm);
 
+struct SegT{
+#define lson rt<<1,l,m
+#define rson rt<<1|1,m+1,r
+#define lc rt<<1
+#define rc rt<<1|1
+	int num[MAX*4];ll sum[MAX*4];
+	void pushup(int rt){
+		sum[rt]=sum[lc]+sum[rc];
+		num[rt]=num[lc]+num[rc];
+	}
+	void build(int rt,int l,int r){
+		if(l==r){
+			num[rt]=sum[rt]=0;
+			return;
+		}
+		int m=(l+r)>>1;
+		build(lson);build(rson);
+		pushup(rt);
+	}
+	void insert(int rt,int l,int r,int p){
+		if(l==r){
+			sum[rt]+=dc[p];
+			num[rt]+=1;
+			return;
+		}
+		int m=(l+r)>>1;
+		if(p<=m) insert(lson,p);
+		else insert(rson,p);
+		pushup(rt);
+	}
+	int query(int rt,int l,int r,ll sm){
+		if(l==r){
+			return min(num[rt],(int)(sm/dc[l]));
+		}
+		int m=(l+r)>>1;
+		if(sum[lc]<=sm) return num[lc]+query(rson,sm-sum[lc]);
+		else return query(lson,sm);
 	}
 
 }T;
 
 
-
 int main(){
-	debug;
+	//debug;
 	scanf("%d",&ks);
 	while(ks--){
-		scanf("%d%lld",&n,&m);
-		T.init();
+		scanf("%d%lld",&n,&k);
 		for(int i=1;i<=n;i++){
 			scanf("%d",&w[i]);
 			dc[i]=w[i];
 		}
 		sort(dc+1,dc+1+n);
 		tot=unique(dc+1,dc+1+n)-(dc+1);
-		T.build(rt[0],1,tot);
+		T.build(1,1,tot);
 		for(int i=1;i<=n;i++){
+		}
+		for(int i=1;i<=n;i++){
+			ll sm=k-w[i];
+			int ans=T.query(1,1,tot,sm);
 			int t=lower_bound(dc+1,dc+1+tot,w[i])-dc;
-			T.insert(rt[i],rt[i-1],1,tot,t);
+			T.insert(1,1,tot,t);
+			printf("%d ",i-1-ans);
 		}
-		for(int i=1;i<=n;i++){
-			ll sm=m-w[i];
-			int ans=T.query(rt[i-1],rt[0],1,tot,sm);
-			printf("%d",i-1-ans);
-			if(i==n) printf("\n");
-			else printf(" ");
-		}
+		printf("\n");
 	}
 	return 0;
 }
-
